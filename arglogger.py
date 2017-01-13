c772cc3bda3e248e871ebc54d64d81cf95a3b561
Tom Elliott <tom.elliott@nyu.edu>
2017-01-13 14:13:05 -0600

    going to want this at some point

---
+++ arglogger.py
@@ -0,0 +1,14 @@
+from functools import wraps
+import logging
+
+
+def arglogger(func):
+    """
+    decorator to log argument calls to functions
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
+        logger = logging.getLogger(func.__name__)
+        logger.debug("called with arguments: %s, %s" % (args, kwargs))
+        return func(*args, **kwargs)
+    return inner
