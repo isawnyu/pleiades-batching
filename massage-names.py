41bfd6140c1d8b04847779ab71dde4e6d21b6317
Tom Elliott <tom.elliott@nyu.edu>
2017-01-18 14:57:14 -0600

    adapt script to use csv utilities module

--- massage-names.py
+++ massage-names.py
@@ -6,6 +6,7 @@ from arglogger import arglogger
 import argparse
 import inspect
 import csv
+from csv_utilities import CSV_DIALECTS
 import json
 import logging
 from names import PleiadesName
@@ -15,34 +16,6 @@ from pprint import pformat
 import re
 import sys
 import traceback
-
-CSV_DIALECTS = {}
-csv.register_dialect(
-    'excel-quote-none',
-    csv.get_dialect('excel'),
-    quoting=csv.QUOTE_NONE)
-csv.register_dialect(
-    'unix-quote-none',
-    csv.get_dialect('unix'),
-    quoting=csv.QUOTE_NONE)
-csv.register_dialect(
-    'excel-escape-quotes',
-    csv.get_dialect('excel'),
-    doublequote=False)
-csv.register_dialect(
-    'unix-escape-quotes',
-    csv.get_dialect('unix'),
-    doublequote=False)
-csv.register_dialect(
-    'excel-quote-all',
-    csv.get_dialect('excel'),
-    quoting=csv.QUOTE_ALL)
-csv.register_dialect(
-    'unix-quote-all',
-    csv.get_dialect('unix'),
-    quoting=csv.QUOTE_ALL)
-for dialect_name in csv.list_dialects():
-    CSV_DIALECTS[dialect_name] = csv.get_dialect(dialect_name)
 
 DEFAULT_LOG_LEVEL = logging.WARNING
 POSITIONAL_ARGUMENTS = [
@@ -165,7 +138,7 @@ def test_csv(fname: str, dialect_arg=None):
             return dialect_arg
         else:
             for k, v in CSV_DIALECTS.items():
-                logger.debug('trying dialect: {}'.format(dialect_name))
+                logger.debug('trying dialect: {}'.format(k))
                 if dialects_match(dialect, v):
                     logger.info(
                         'CSV file {} has dialect "{}".'.format(fname, k))
